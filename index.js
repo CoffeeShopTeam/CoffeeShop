8ab1f500dcb68e6d5f90e25285bacad675284621
roee zach <roeezach15@gmail.com>
2023-06-10 18:22:59 +0300

    add mongo connect configurations

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require("express");
+const mongoConnect = require('./src/config/mongoConnect.js');
 const app = express();
-const PORT = 54321
 
+const PORT = process.env.PORT;
 app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
+    mongoConnect();
 })
